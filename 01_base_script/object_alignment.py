df38a0433444f9767ab907fb59b22519e2186e8d
haruyasu <harukun2002@gmail.com>
2018-02-10 20:56:30 -0800

    object alignment

--- 01_base_script/object_alignment.py
+++ 01_base_script/object_alignment.py
@@ -1,3 +1,16 @@
 import maya.cmds as mc
-objAligner = mc.window(title="Object Aligment Tool", s=False, wh={300, 100})
-mc.column
+
+string = objAligner
+
+if mc.window(objAligner, ex=True):
+    mc.deleteUI(objAligner, window=True)
+
+objAligner = mc.window(title="Object Aligment Tool", s=False, wh=(300, 100))
+mc.columnLayout(adj=True)
+mc.text(l="Instructions: select source, then target")
+mc.button(l="Go Aligner Go!", w=300, h=100, c="aligner()")
+mc.showWindow(objAligner)
+
+def aligner():
+    prtCns = mc.parentConstraint()
+    mc.delete(prtCns)
