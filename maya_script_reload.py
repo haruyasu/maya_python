125edbf4ea6ee612970e7a4829cff1854bbbd46b
haruyasu <harukun2002@gmail.com>
2018-07-16 10:29:32 -0400

    Preset UI

--- maya_script_reload.py
+++ maya_script_reload.py
@@ -7,3 +7,15 @@ if not path in sys.path:
 
 import table_view
 reload(table_view)
+
+############
+import pymel.core as pm
+import sys
+
+path = "D:/github/maya_python/06_tools"
+if not path in sys.path:
+	sys.path.append(path)
+
+import preset_ui
+reload(preset_ui)
+preset_ui.PresetUI()
